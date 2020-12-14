03c19729dc8fa9af723cfa11479b8f6e942cb8d5
Novikov Anton <anton1995novikov@gmail.com>
2020-12-14 11:53:07 +0200

    feat: lesson03 complete | NavBar.js

---
+++ mrdonalds/src/Components/NavBar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+import logoImg from '../images/logo.svg';
+import SignImg from '../images/sign.svg';
+
+const NavBarStyled = styled.header`
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: 0;
+	background: #299b01;
+	height: 80px;
+	width: 100vw;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 15px;
+	color: white;
+`;
+
+const Logo = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const H1 = styled.h1`
+	font-size: 24px;
+	margin-left: 15px;
+`;
+
+const ImgLogo = styled.img`
+	widht: 50px;
+`;
+
+const Login = styled.button`
+	outline: none;
+	border: none;
+	text-transform: uppercase;
+	font-family: Roboto, sans-serif;
+	background: transparent;
+	cursor: pointer;
+	color: #fff;
+	img {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 3px;
+	}
+`;
+
+export const NavBar = () => (
+	<NavBarStyled>
+		<Logo>
+			<ImgLogo src={logoImg} alt='logo' />
+			<H1>MrDonald's</H1>
+		</Logo>
+		<Login>
+			<img src={SignImg} alt='login' />
+			Войти
+		</Login>
+	</NavBarStyled>
+);
